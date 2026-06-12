1587a477c479d95f1d275ff6c7f43f8b5bdaf8eb
SonaamMourya04 <sonammourya7683@gmail.com>
2026-06-12 21:36:24 +0530

    inheritance

---
+++ Inhert.java
@@ -0,0 +1,12 @@
+  
+
+
+public class Inhert {
+    public static void main(String a[]){
+         Clac obj=new Clac();
+         int r1=obj.add(4,5);
+         int r2=obj.sub(7,3);
+         System.out.println(r1 +" "+r2);
+
+    }
+}
